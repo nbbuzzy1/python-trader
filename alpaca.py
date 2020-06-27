f5314d544d8d427cfe3e0a17931ec2ec6bf77bec
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-06-27 07:48:19 -0400

    Set up grabbing last trade and historic data.

---
+++ alpaca.py
@@ -0,0 +1,5 @@
+import os
+
+alpaca_api_key = os.environ.get('ALPACA_API')
+alpaca_secret = os.environ.get('ALPACA_SECRET')
+alpaca_endpoint = 'https://paper-api.alpaca.markets'
--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-# Trading Bot Using Alpaca
-
-import os
 import alpaca_trade_api as tradeapi
-
-
-alpaca_api_key = os.environ.get('ALPACA_API')
-alpaca_secret = os.environ.get('ALPACA_SECRET')
-alpaca_endpoint = 'https://paper-api.alpaca.markets'
+from alpaca import alpaca_api_key, alpaca_secret, alpaca_endpoint
 
 api = tradeapi.REST(
     alpaca_api_key,
@@ -16,4 +9,8 @@ api = tradeapi.REST(
 
 # Get our account information.
 account = api.get_account()
-print(account)
+
+print(api.get_last_trade('MSFT'))
+aapl = api.polygon.historic_agg_v2(
+    'AAPL', 1, 'day', _from='2019-01-01', to='2019-02-01').df
+print(aapl)
