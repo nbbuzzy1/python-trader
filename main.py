38b4c377867156caf4a257ba3b49842516b0158d
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-06-28 08:21:07 -0400

    Testing how a stock does after two losing days.

--- main.py
+++ main.py
@@ -10,7 +10,27 @@ api = tradeapi.REST(
 # Get our account information.
 account = api.get_account()
 
-print(api.get_last_trade('MSFT'))
+# print(api.get_last_trade('MSFT'))
 aapl = api.polygon.historic_agg_v2(
-    'AAPL', 1, 'day', _from='2019-01-01', to='2019-02-01').df
-print(aapl)
+    'DOW', 1, 'day', _from='2019-01-01', to='2019-12-31').df
+
+
+# Given a security, check how it did on the third day after decreasing the prior two days?
+closing_values = list(aapl.get('close'))
+sum_dollar = 0
+sum_percent = 0
+
+for index, value in enumerate(closing_values):
+    if index < 3:
+        continue
+    else:
+        if closing_values[index - 3] > closing_values[index - 2] > closing_values[index - 1]:
+            percent = round(
+                ((value - closing_values[index - 1]) / closing_values[index - 1]), 2)
+            sum_percent += percent
+            dollar_change = value - closing_values[index - 1]
+            sum_dollar += dollar_change
+
+# print(percent_increases)
+print(sum_dollar)
+print(sum_percent)
